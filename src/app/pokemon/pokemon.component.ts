ab7c69fde06980b3f39e2e13da9f3a69fa236432
Lucas Emmanuel <lucasemanuelalves@hotmail.com>
2021-11-17 10:20:41 -0300

    Input com pesquisa

--- src/app/pokemon/pokemon.component.ts
+++ src/app/pokemon/pokemon.component.ts
@@ -1,10 +1,8 @@
 
 
-import { map } from 'rxjs/operators';
-import { ListaPokemonComponent } from './../lista-pokemon/lista-pokemon.component';
+
 import { Component } from '@angular/core';
 import { Pokemon } from '../_module/Pokemon';
-import { Tipos } from '../_module/Tipos';
 import { PokemonService } from '../_services/pokemon.service';
 
 @Component({
@@ -20,6 +18,7 @@ export class PokemonComponent {
   public pokemon: Pokemon[] = [];
   public isDisplay = false;
   public isDisplayF = false;
+  public pF: any = [];
 
   constructor(
     public pokemonService: PokemonService,
@@ -31,8 +30,6 @@ export class PokemonComponent {
     this.pokemonService.listaPokemon.subscribe(
       res => {
         this.lista = res.results;
-
-
       }
     );
     this.isDisplay = true;
@@ -48,17 +45,55 @@ export class PokemonComponent {
     )
 
   }
-
-  listarPorTipoF() {
-    this.isDisplayF = !this.isDisplayF;
-    this.listaFiltrada = [];
-    this.pokemonService.listaPokemon.subscribe((res: any) => {
-      this.listaFiltrada = res.results;
-    }
-    )
-
+  filtroI(event: any){
+    console.log("teste", event.target.value);
+    this.isDisplayF = false;
+      this.pF = [];
+      if(event){
+      this.lista.map((p: any)=>{
+          if(p.status){
+            console.log('Status:',p.status);
+            if(p.status.types){
+              p.status.types.map((t: any)=>{
+                console.log('Types: ',t);
+                console.log(t.type.name)
+                if(t.type.name === event.target.value){
+                  this.pF.push(p);
+                }
+              })
+            }
+          }
+         
+        })
+        console.log(this.pF);
+      }
+      else(this.pF = this.lista)
   }
 
+  filtro(key?: string) {
+    this.isDisplayF = false;
+      this.pF = [];
+      if(key){
+      this.lista.map((p: any)=>{
+          if(p.status){
+            console.log('Status:',p.status);
+            if(p.status.types){
+              p.status.types.map((t: any)=>{
+                console.log('Types: ',t);
+                console.log(t.type.name)
+                if(t.type.name === key){
+                  this.pF.push(p);
+                }
+              })
+            }
+          }
+         
+        })
+        console.log(this.pF);
+      }
+      else(this.pF = this.lista)
+    
+  }
 
   listarPorJogo() {
     this.listaFiltrada = [];
