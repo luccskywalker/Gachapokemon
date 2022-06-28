557798671c4f82ab0d827c1c5d9e820899b8351f
Lucas Emmanuel <lucasemanuelalves@hotmail.com>
2022-06-28 20:51:47 -0300

    Teste loading do subscribe

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { finalize } from 'rxjs';
+import { PokemonService } from '../_services/pokemon.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,37 @@ import { Router } from '@angular/router';
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-  constructor(private router: Router) {}
-
+  constructor(private router: Router, private pokemonServico: PokemonService) {}
+  public isLoading = false;
   ngOnInit(): void {}
+
+  public testeSubscribe() {
+    this.isLoading = true;
+    this.pokemonServico.listaPokemon
+      .pipe(
+        finalize(() => {
+          console.log('entrou no segundo subscribe');
+          this.pokemonServico.pegaTipos
+            .pipe(
+              finalize(() => {
+                this.isLoading = false;
+              })
+            )
+            .subscribe(
+              (res2: any) => {
+                console.log(res2);
+              },
+              (err: any) => {
+                console.log(err);
+              }
+            );
+        })
+      )
+      .subscribe((res: any) => {
+        console.log('entrou no primeiro subscribe');
+        console.log(res);
+      });
+  }
 
   gotoList() {
     console.log('entrou');
